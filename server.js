aebeb0084ac4e63c35dc87666235777341b861ba
EduardOrellana <ore23006@byui.edu>
2024-10-04 20:29:57 -0600

    error handling

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute");
 //adding the controller
 const baseController = require("./controllers/baseController");
+const utilities = require("./utilities/index"); //to get the error handle.
 
 //the option to charge the layouts.
 app.set("view engine", "ejs")
@@ -31,6 +32,27 @@ app.get("/", baseController.buildHome)
 //Inventory Controller
 app.use("/inv", inventoryRoute)
 app.use(static)
+
+/*
+Error Handling 
+*/
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error.ejs", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
